c10f326a4f647efc583f996934fe9c0f861d5c00
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2017-04-09 05:19:13 +0000

    Multiples dependencias

---
+++ tests/MultipleDependenciesTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MultipleDependenciesTest extends TestCase
+{
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+    public function testConsurmer()
+    {
+        $this->assertEquals(
+            ['first', 'second'],
+            func_get_args()
+        );
+    }
+}
